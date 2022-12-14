59c6129393c121fbe6d2124a6415ad3110c1dc1b
daniel <53857925+dalnel@users.noreply.github.com>
2022-12-15 01:20:16 +0800

    2022.12.15
    開放複習模式，及美化圖面

--- jump.js
+++ jump.js
@@ -1,11 +1,14 @@
 $("#test").click(function () {
-    $(location).attr("href","./pageChoice.html")
+    //$(location).attr("href","./pageChoice.html?select=1");
+    alert("即將推出，目前僅開放複習");
 })
 
 $("#review").click(function () {
-    $(location).attr("href","./pageChoice.html")
+    $(location).attr("href","./pageChoice.html?select=2");
 })
 
 $("#creat").click(function () {
-    $(location).attr("href","./pageChoice.html")
+    //$(location).attr("href","./pageChoice.html?select=3");
+    alert("即將推出，目前僅開放複習");
+
 })
--- pageChoice.js
+++ pageChoice.js
@@ -1,4 +1,46 @@
 
 $("#send").click(function (){
-    document.location.href ='review.html?sp=' + $("#sp").val() + '&ep=' + $("#ep").val() + '&rn=' + $("#review_number").val();  
+    
+    if(select == "1")
+    {
+        console.log("1");
+        //document.location.href ='review.html?sp=' + $("#sp").val() + '&ep=' + $("#ep").val() + '&rn=' + $("#review_number").val();  
+    }
+    else if(select == "2")
+    {
+        console.log("2");
+        var sp = $("#sp").val() - 10;
+        var ep = $("#ep").val() - 10
+        document.location.href ='review.html?sp=' + sp + '&ep=' + ep + '&rn=' + $("#review_number").val();      
+    }
+    else if(select == "3")
+    {
+        console.log("3");
+        var sp = $("#sp").val() - 10;
+        var ep = $("#ep").val() - 10
+        //document.location.href ='review.html?sp=' + sp + '&ep=' + ep + '&rn=' + $("#review_number").val();  
+    }
+
 })
+
+function get() {
+    var url = location.href;
+    
+    //取得問號之後的值
+    var temp = url.split("?");
+
+    //將值再度分開
+    var vars = temp[1].split("&");
+
+    //一一顯示出來
+    for (var i = 0; i < vars.length; i++) {
+     console.log(vars[i]);
+     
+    };
+    console.log(vars[0].split("select=")[1]);
+    return vars[0].split("select=")[1];
+
+
+
+}
+var select = get()
